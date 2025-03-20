b54295c45ae2b32668dbb0e90b5cfad3911921ef
Vadim Antipin <vantipin.codica@gmail.com>
2025-03-20 18:35:52 +0200

    [Feature] Add shadcn and theme

--- src/app/decks/[id]/page.tsx
+++ src/app/decks/[id]/page.tsx
@@ -3,23 +3,25 @@ import { getClient } from "@/lib/ApolloClient";
 import Flashcard from "@/components/Flashcard";
 
 const query = gql`
-  query GetFlashcards {
-    flashcards {
-      id
-      question
-      answer
+  query Deck($id: ID!) {
+    deck(id: $id) {
+      flashcards {
+        id
+        question
+        answer
+      }
     }
   }
 `;
 
-export default async function Home() {
-  const { data } = await getClient().query({ query });
+export default async function Home({ params }: { params: { id: string } }) {
+  const { data } = await getClient().query({ query, variables: { id: params.id } });
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-4xl font-bold text-white mb-8">Flashcards</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
-        {data.flashcards.map((flashcard: { id: number; question: string; answer: string }) => (
+        {data.deck.flashcards.map((flashcard: { id: number; question: string; answer: string }) => (
           <Flashcard key={flashcard.id} question={flashcard.question} answer={flashcard.answer} />
         ))}
       </div>
--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Card from "./Card";
+import { Card, CardContent, CardFooter } from "./ui/card";
 
 type DeckProps = {
   name: string;
@@ -11,12 +11,16 @@ type DeckProps = {
 export default function Deck({ name, deckId }: DeckProps) {
   return (
     <Link href={`/decks/${deckId}`} className="block w-full">
-      <Card className="transition-all duration-300 hover:transform hover:scale-105 hover:shadow-2xl">
-        <h2 className="text-2xl font-bold text-white text-center mb-2">{name}</h2>
+      <Card className="transition-all duration-300 hover:transform hover:scale-105 hover:shadow-2xl group">
+        <CardContent className="flex flex-col items-center justify-center">
+          <h2 className="text-2xl font-bold text-center mb-2">{name}</h2>
+        </CardContent>
         
-        <span className="text-gray-400 text-sm mt-4 group-hover:text-blue-400 transition-colors duration-300">
-          Click to view cards
-        </span>
+        <CardFooter className="justify-center">
+          <span className="text-gray-400 text-sm group-hover:text-blue-400 transition-colors duration-300">
+            Click to view cards
+          </span>
+        </CardFooter>
       </Card>
     </Link>
   );
